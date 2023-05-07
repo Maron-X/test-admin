e21795e5a9a857f713ad0ecb47f0a376a7e89b74
Maron-X <120732521+Maron-X@users.noreply.github.com>
2023-05-07 13:56:10 +0300

    refresh

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,22 +1,20 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  target: "static",
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'PersonX Admin Panel',
+    title: "PersonX Admin Panel",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
     htmlAttrs: {
-      'data-theme': 'forest',
-    }
+      "data-theme": "forest",
+    },
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -26,12 +24,8 @@ export default {
     "~static/fontaswesome/css/fontawesome.min.css",
   ],
 
-
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-
-    '~/plugins/mixins'
-  ],
+  plugins: ["~/plugins/mixins"],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -40,19 +34,19 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
     "@nuxtjs/color-mode",
-    '@nuxtjs/tailwindcss',
+    "@nuxtjs/tailwindcss",
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
+    "@nuxtjs/axios",
     // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa',
+    "@nuxtjs/pwa",
     "vue-toastification/nuxt",
 
     [
-      '@nuxtjs/firebase',
+      "@nuxtjs/firebase",
       {
         config: {
           apiKey: "AIzaSyA2HiaA6qhtzyjjH6wC0BVqeOJlAPEsHNI",
@@ -61,39 +55,36 @@ export default {
           storageBucket: "personx-clients.appspot.com",
           messagingSenderId: "404892688464",
           appId: "1:404892688464:web:962751c543da7a488ef155",
-          measurementId: "G-THXDFE5QPW"
+          measurementId: "G-THXDFE5QPW",
         },
         services: {
           firestore: true,
           database: true,
           auth: {
-            persistence: 'local', // default
+            persistence: "local", // default
             initialize: {
-              onAuthStateChangedAction: 'onAuthStateChangedAction',
-              subscribeManually: false
+              onAuthStateChangedAction: "onAuthStateChangedAction",
+              subscribeManually: false,
             },
             ssr: false, // default
           }, // Just as example. Can be any other service.
-
-        }
-      }
+          // }, // Just as example. Can be any other service.
+        },
+      },
     ],
-
-
   ],
 
-  layout: 'panel',
+  layout: "panel",
 
   router: {
-    middleware: 'auth'
+    middleware: "auth",
   },
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: "/",
   },
-
 
   toast: {
     // Vue Toastification plugin options
@@ -104,17 +95,15 @@ export default {
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'en'
-    }
+      lang: "en",
+    },
   },
-
 
   generate: {
     fallback: true,
-    dir: 'dist'
+    dir: "dist",
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {},
+};
